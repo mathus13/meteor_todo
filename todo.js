2f47a3741080d9a97201dd99cf38913fc1217666
sbarratt <mathus13@gmail.com>
2016-03-19 20:55:39 -0400

    quick todo
    created a quick todo app following the tutorial found here: https://www.meteor.com/tutorials/blaze/creating-an-app

---
+++ todo.js
@@ -0,0 +1,46 @@
+'use strict';
+var Tasks = new Mongo.Collection("tasks");
+if (Meteor.isClient) {
+    // counter starts at 0
+    Template.body.helpers({
+        tasks: function () {
+            return Tasks.find({}, {sort: {checked: 1, createdAt: -1}});
+        }
+    });
+    Template.body.events({
+        "submit .new-task": function (event) {
+            // Prevent default browser form submit
+            event.preventDefault();
+
+            // Get value from form element
+            var text = event.target.text.value;
+
+            // Insert a task into the collection
+            Tasks.insert({
+                text: text,
+                createdAt: new Date() // current time
+            });
+
+            // Clear form
+            event.target.text.value = "";
+        }
+    });
+
+    Template.task.events({
+        "click .toggle-checked": function () {
+            // Set the checked property to the opposite of its current value
+            Tasks.update(this._id, {
+                $set: {checked: !this.checked}
+            });
+        },
+        "click .delete": function () {
+            Tasks.remove(this._id);
+        }
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
+}
